8c9aedd11609c9c69cb0ed62e198d79204b0786d
bendtherules <abhasbhattacharya2@gmail.com>
2023-11-28 13:48:10 +0530

    Build

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER 29
+  #define BUILD_NUMBER 30
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.29 - 2023-11-28 13:45:20.821565"
+  #define VERSION "v0.1.30 - 2023-11-28 13:47:56.143755"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.29"
+  #define VERSION_SHORT "v0.1.30"
 #endif
