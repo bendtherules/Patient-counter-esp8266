f88f38c8e83bdc0b22cff346e51f084e7ac405a3
bendtherules <abhasbhattacharya2@gmail.com>
2023-11-28 09:10:27 +0530

    Add build number pre-script

---
+++ include/version.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER 1
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.1 - 2023-11-28 09:09:49.089072"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.1"
+#endif
