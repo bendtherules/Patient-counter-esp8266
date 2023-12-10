19af05ec6e7035f6daa138f221db15fc1fd1f32b
bendtherules <abhasbhattacharya2@gmail.com>
2023-12-10 11:53:56 +0530

    Make right blue

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER 74
+  #define BUILD_NUMBER 75
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.74 - 2023-12-10 11:50:48.393610"
+  #define VERSION "v0.1.75 - 2023-12-10 11:53:08.758201"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.74"
+  #define VERSION_SHORT "v0.1.75"
 #endif
